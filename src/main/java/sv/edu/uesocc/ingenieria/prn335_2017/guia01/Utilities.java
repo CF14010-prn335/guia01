13f7b93f3915e1abedd44b01abfeebc63d3064d1
PC03 <PC03@172.16.97.106>
2017-08-17 11:40:56 -0600

    Guia 01

---
+++ src/main/java/sv/edu/uesocc/ingenieria/prn335_2017/guia01/Utilities.java
@@ -0,0 +1,18 @@
+
+package sv.edu.uesocc.ingenieria.prn335_2017.guia01;
+
+public class Utilities {
+ 
+public String getResume (String texto){
+    return "";
+}    
+    
+ public String Capitalizar (String texto){
+     return "";
+ }   
+    
+  public int ContarCoincidencias(String frase, String texto){
+     return 0; 
+  }  
+    
+}
