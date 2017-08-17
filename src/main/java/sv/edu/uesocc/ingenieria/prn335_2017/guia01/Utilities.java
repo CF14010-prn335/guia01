67ae048cced96cfa2bd5bad91896c8c3ec3bc3f9
PC03 <PC03@172.16.97.106>
2017-08-17 12:06:56 -0600

    Met 1

--- src/main/java/sv/edu/uesocc/ingenieria/prn335_2017/guia01/Utilities.java
+++ src/main/java/sv/edu/uesocc/ingenieria/prn335_2017/guia01/Utilities.java
@@ -4,7 +4,19 @@ package sv.edu.uesocc.ingenieria.prn335_2017.guia01;
 public class Utilities {
  
 public String getResume (String texto){
-    return "";
+    String caracter ="";
+    int incremento =0;
+    boolean existencia = false;
+    do{
+      caracter=caracter+texto.charAt(incremento);
+      if (incremento<=texto.length()){
+          existencia=false;
+      } else{
+       existencia=true;
+ }
+      
+    }while(existencia==false);
+    return caracter;
 }    
     
  public String Capitalizar (String texto){
